3890b8fc18f6226ec56b856b8ac14b23a85fbeed
Pugnani <pugnani1906@gmail.com>
2024-06-30 18:17:02 +0200

    randomizer more the attack

--- src/main/java/CheckboardStrategy.java
+++ src/main/java/CheckboardStrategy.java
@@ -1,17 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+import java.util.Random;
+
 public class CheckboardStrategy implements AttackStrategy{
 
     //attack as a "chess board", if all the "chess position" are attacked use bestmove from heat map
+    private Random random = new Random();
+
     @Override
     public int[] getNextMove(Heatmap_Impl heatmap, BoardFeedback board) {
         int width = board.getWidth();
         int height = board.getHeight();
-        for(int x=0;x<width;x++){
-            for(int y=0;y<height;y++){
-                if((x+y)%2==0 && !board.isAlreadyAttacked(x,y)){
-                    return new int[]{x,y};
+
+        List<int[]> validPositions = new ArrayList<>();
+
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                if ((x + y) % 2 == 0) {
+                    validPositions.add(new int[]{x, y});
                 }
             }
         }
-        return heatmap.getBestMove(); //if all position already attacked
+
+        Collections.shuffle(validPositions, random);
+
+        for (int[] pos : validPositions) {
+            if (!board.isAlreadyAttacked(pos[0], pos[1])) {
+                return pos;
+            }
+        }
+
+        return heatmap.getBestMove(); // if all positions are already attacked
     }
 }
+
--- src/main/java/SpiralStrategy.java
+++ src/main/java/SpiralStrategy.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class SpiralStrategy implements AttackStrategy{
     @Override
     public int[] getNextMove(Heatmap_Impl heatmap, BoardFeedback board) {
@@ -5,16 +7,23 @@ public class SpiralStrategy implements AttackStrategy{
         int height = board.getHeight();
         int centerX = width / 2;
         int centerY = height / 2;
+        Random rand = new Random();
 
         for (int layer = 0; layer <= Math.max(centerX, centerY); layer++) {
-            for (int i = -layer; i <= layer; i++) {
+            // Randomize the order of moves
+            int[] indices = rand.ints(-layer, layer + 1).distinct().limit(2 * layer + 1).toArray();
+
+            for (int i : indices) {
                 int x = centerX + i;
                 int y1 = centerY + layer;
                 int y2 = centerY - layer;
                 if (isValidMove(x, y1, width, height, board)) return new int[]{x, y1};
                 if (isValidMove(x, y2, width, height, board)) return new int[]{x, y2};
             }
-            for (int j = -layer + 1; j < layer; j++) {
+
+            indices = rand.ints(-layer + 1, layer).distinct().limit(2 * layer - 1).toArray();
+
+            for (int j : indices) {
                 int y = centerY + j;
                 int x1 = centerX + layer;
                 int x2 = centerX - layer;
@@ -22,6 +31,7 @@ public class SpiralStrategy implements AttackStrategy{
                 if (isValidMove(x2, y, width, height, board)) return new int[]{x2, y};
             }
         }
+
         return heatmap.getBestMove(); // if position already attacked
     }
 
